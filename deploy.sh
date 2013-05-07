4da4063b5c1d3786271c8a8bf7a833a599958020
Jonas Gorauskas <jgorauskas@gmail.com>
2013-05-06 18:24:11 -0700

    Created deploy script; fixed conflicting aliases; fixed open function

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Quick script to deploy dot files to the correct location
+# Created: 2013-05-06 17:39:39 by Jonas Gorauskas [JGG]
+# Modified: 2013-05-06 17:53 by jgg
+
+SOURCE="${BASH_SOURCE[0]}"
+
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+
+  # if $SOURCE was a relative symlink, we need to resolve it
+  # relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+read -p "
+This deployment script will copy relevant dot files
+from $DIR to $HOME
+Proceed? [Y/n] " YN
+
+while true; do
+    case $YN in
+        [Yy]*) break;;
+        [Nn]*) echo "Aborting..."; exit;;
+        *) read -p "Please answer Y or N!!! " YN ;;
+    esac
+done
+
+if [ -d $DIR ]; then
+    for SHFILE in `ls -1 $DIR/.bash* $DIR/.in* $DIR/.pro* $DIR/.vimrc $DIR/.[xX]*`; do
+        echo "Copying $SHFILE to $HOME/ ..."
+        cp $SHFILE $HOME
+    done
+fi
+
+echo "Copying $DIR/.vim to $HOME/ ..."
+cp -r $DIR/.vim $HOME
+
+echo "All done!"
