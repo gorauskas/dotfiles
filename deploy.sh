91cb2f792825eff4cd699170fb3f1c7cdb657ff6
Jonas Gorauskas <jgorauskas@gmail.com>
2013-12-05 01:01:16 -0800

    fixed corner cases on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Quick script to deploy dot files to the correct location
 # Created: 2013-05-06 17:39:39 by Jonas Gorauskas [JGG]
-# Modified: 2013-05-06 17:53 by jgg
+# Modified: 2013-12-04 22:26 by jgg
 
 SOURCE="${BASH_SOURCE[0]}"
 
@@ -38,5 +38,17 @@ fi
 
 echo "Copying $DIR/.vim to $HOME/ ..."
 cp -r $DIR/.vim $HOME
+
+echo "Copying $DIR/.gitconfig to $HOME/ ..."
+cp $DIR/.gitconfig $HOME
+
+echo "Copying $DIR/.gitk to $HOME/ ..."
+cp $DIR/.gitk $HOME
+
+echo "Copying $DIR/.conkyrc to $HOME/ ..."
+cp $DIR/.conkyrc $HOME
+
+echo "Copying $DIR/mercurial.ini to $HOME/ ..."
+cp $DIR/mercurial.ini $HOME
 
 echo "All done!"
