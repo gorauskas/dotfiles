5df8fce5646cad5e56a2f5b988e6a6f8fa36aae7
Jonas Gorauskas <jgorauskas@gmail.com>
2014-09-21 12:53:04 -0700

    Remove interactive ask for deploy type

--- deploy.sh
+++ deploy.sh
@@ -1,65 +1,59 @@
 #!/usr/bin/env bash
+# Created: 2013-05-06 17:39:39 by Jonas Gorauskas [JGG]
+# Modified: 2014-09-21 12:53:01
 
 # The strategy is to git clone the repo to ~/dotfiles and then execute this file
 # to generate sym links on ~/ to all the files in the repo. This file is
 # intended for use in Linux.
-# Created: 2013-05-06 17:39:39 by Jonas Gorauskas [JGG]
-# Modified: 2014-09-07 03:10:16
+
+if [ $# -eq 0 ]; then
+    echo "ERROR: No arguments!"
+    exit 1
+fi
 
 DF_DIR=${PWD}
 
-read -p "
-Is this system a server? [Y/N] " SYN
+ln -v -fs ${DF_DIR}/.bash_alias ${HOME}/.bash_alias
+ln -v -fs ${DF_DIR}/.bash_function ${HOME}/.bash_function
+ln -v -fs ${DF_DIR}/.bash_logout ${HOME}/.bash_logout
+ln -v -fs ${DF_DIR}/.bash_profile ${HOME}/.bash_profile
+ln -v -fs ${DF_DIR}/.bash_prompt ${HOME}/.bash_prompt
+ln -v -fs ${DF_DIR}/.bashrc ${HOME}/.bashrc
+ln -v -fs ${DF_DIR}/.gitconfig ${HOME}/.gitconfig
+ln -v -fs ${DF_DIR}/.inputrc ${HOME}/.inputrc
+ln -v -fs ${DF_DIR}/mercurial.ini ${HOME}/mercurial.ini
+ln -v -fs ${DF_DIR}/.tmux.conf ${HOME}/.tmux.conf
+ln -v -fs ${DF_DIR}/.vimrc ${HOME}/.vimrc
 
-read -p "
-This deployment script will symlink relevant dot files
-from $DF_DIR to $HOME
-Proceed? [Y/N] " YN
+[ -h ${HOME}/.vim ] && rm ${HOME}/.vim
+ln -v -fs ${DF_DIR}/.vim/ ${HOME}/.vim
 
-if [[ "$YN" == "y" || "$YN" == "Y" ]]; then
-    ln -v -fs ${DF_DIR}/.bash_alias ${HOME}/.bash_alias
-    ln -v -fs ${DF_DIR}/.bash_function ${HOME}/.bash_function
-    ln -v -fs ${DF_DIR}/.bash_logout ${HOME}/.bash_logout
-    ln -v -fs ${DF_DIR}/.bash_profile ${HOME}/.bash_profile
-    ln -v -fs ${DF_DIR}/.bash_prompt ${HOME}/.bash_prompt
-    ln -v -fs ${DF_DIR}/.bashrc ${HOME}/.bashrc
-    ln -v -fs ${DF_DIR}/.gitconfig ${HOME}/.gitconfig
-    ln -v -fs ${DF_DIR}/.inputrc ${HOME}/.inputrc
-    ln -v -fs ${DF_DIR}/mercurial.ini ${HOME}/mercurial.ini
-    ln -v -fs ${DF_DIR}/.tmux.conf ${HOME}/.tmux.conf
-    ln -v -fs ${DF_DIR}/.vimrc ${HOME}/.vimrc
+if [[ "$1" == "desktop" ]]; then
+    ln -v -fs ${DF_DIR}/.conkyrc ${HOME}/.conkyrc
+    ln -v -fs ${DF_DIR}/.gitk ${HOME}/.gitk
+    ln -v -fs ${DF_DIR}/.Xresources ${HOME}/.Xresources
 
-    [ -h ${HOME}/.vim ] && rm ${HOME}/.vim
-    ln -v -fs ${DF_DIR}/.vim/ ${HOME}/.vim
+    [ -h ${HOME}/.idlerc ] && rm ${HOME}/.idlerc
+    ln -v -fs ${DF_DIR}/.idlerc/ ${HOME}/.idlerc
 
-    if [[ "$SYN" == "n" || "$SYN" == "N" ]]; then
-        ln -v -fs ${DF_DIR}/.conkyrc ${HOME}/.conkyrc
-        ln -v -fs ${DF_DIR}/.gitk ${HOME}/.gitk
-        ln -v -fs ${DF_DIR}/.Xresources ${HOME}/.Xresources
-
-        [ -h ${HOME}/.idlerc ] && rm ${HOME}/.idlerc
-        ln -v -fs ${DF_DIR}/.idlerc/ ${HOME}/.idlerc
-
-        if [ ! -d "${HOME}/bin" ]; then
-            mkdir ${HOME}/bin
-        fi
-        cp ${DF_DIR}/urxvt-launch.sh ${HOME}/bin/urxvt-launch.sh
-
-        if [ ! -d "${HOME}/Pictures" ]; then
-            mkdir ${HOME}/Pictures
-        fi
-        cp -r ${DF_DIR}/tiles/ ${HOME}/Pictures/
-
-        printf "%s\n" "" "if [ \"\$(lsb_release -si)\" == \"Arch\" ]; then" "    linux_logo -b -f -u -L 1" "else" "    linux_logo -b -f -u -L banner-simp" "fi" "" >> ./.bashrc;
-
-        printf "\nprompt fancy\n\n#OEF\n" >> ./.bashrc;
-    else
-        printf "\nprompt def\n\n#EOF\n" >> ./.bashrc
+    if [ ! -d "${HOME}/bin" ]; then
+        mkdir ${HOME}/bin
     fi
+    cp ${DF_DIR}/urxvt-launch.sh ${HOME}/bin/urxvt-launch.sh
 
-    echo "All Done!"
-elif [[ "$YN" == "n" || "$YN" == "N" ]]; then
-    echo "Aborting..."
+    if [ ! -d "${HOME}/Pictures" ]; then
+        mkdir ${HOME}/Pictures
+    fi
+    cp -r ${DF_DIR}/tiles/ ${HOME}/Pictures/
+
+    printf "%s\n" "" "if [ \"\$(lsb_release -si)\" == \"Arch\" ]; then" "    linux_logo -b -f -u -L 1" "else" "    linux_logo -b -f -u -L banner-simp" "fi" "" >> ./.bashrc;
+
+    printf "\nprompt fancy\n\n#OEF\n" >> ./.bashrc;
+elif [[ "$1" == "server" ]]; then
+    printf "\nprompt def\n\n#EOF\n" >> ./.bashrc
 else
-    echo "ERROR: Invalid choice! Aborting..."
+    echo "ERROR: Unknown system type $1"
+    exit 1
 fi
+
+exit 0
